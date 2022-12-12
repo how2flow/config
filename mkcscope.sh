4e33f0b0546b8e99215942a661bd3208a5e640b1
steve.jeong <jkhpro1003@gmail.com>
2022-12-12 16:04:38 +0900

    Add mkcscope.
    Signed-off-by: steve.jeong <jkhpro1003@gmail.com>

---
+++ mkcscope.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# S82-vi-mkcscope
+rm -rf cscope.files cscope.out
+find . \( -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.    S' \) -print > cscope.files
+
+cscope -i cscope.files
